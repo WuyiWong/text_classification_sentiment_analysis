c8cdb275873dc8bfa002a5e692168ccc04703989
Wuyi_WANG <wuyiwong5@gmail.com>
2024-10-10 16:43:47 +0800

    数据准备

---
+++ src/data_preparation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
+
---
+++ src/main.py
@@ -0,0 +1,7 @@
+# 这个脚本是用来书写 text classfication and sentiment analysis的主脚本
+
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
